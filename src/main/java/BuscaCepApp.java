49585b1824b995ac6065c9e5ec74afc5a4cd2aea
leonildoWebRadar <leonildo.azevedo@webradar.com>
2021-02-14 20:32:59 -0300

    Microsserviço funcional

---
+++ src/main/java/BuscaCepApp.java
@@ -0,0 +1,36 @@
+import io.dropwizard.Application;
+import io.dropwizard.client.JerseyClientBuilder;
+import io.dropwizard.setup.Environment;
+import service.BuscaCEP;
+import resources.Recursos;
+
+import javax.ws.rs.client.Client;
+
+/**
+ * Classe main da aplicação
+ * @author Leonildo
+ */
+public class BuscaCepApp extends Application<BuscaCepConfig> {
+
+    public static void main(String[] args) throws Exception {
+        new BuscaCepApp().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "busca-cep";
+    }
+
+    @Override
+    public void run(BuscaCepConfig buscaCepConfig, Environment environment) throws Exception {
+        Client client = new JerseyClientBuilder(environment).using(buscaCepConfig.getHttpClient()).build(getName());
+        BuscaCEP buscaCEP = new BuscaCEP(buscaCepConfig.getTarget(), client, environment.getObjectMapper());
+
+        Recursos cepResource = new Recursos(buscaCEP);
+        environment.jersey().register(cepResource);
+
+        //CorreioControlHealthCheck correioControlHealthCheck = new CorreioControlHealthCheck(cepService);
+        //environment.healthChecks().register("correiocontrol", correioControlHealthCheck);
+    }
+
+}
---
+++ src/main/java/resources/Recursos.java
@@ -0,0 +1,46 @@
+package resources;
+
+import com.codahale.metrics.annotation.Timed;
+import org.hibernate.validator.constraints.Length;
+import model.Endereco;
+import service.BuscaCEP;
+
+import javax.validation.constraints.Digits;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/busca-cep")
+@Produces("application/json; charset=UTF-8")
+public class Recursos {
+
+    private final BuscaCEP buscaCEP;
+
+    /**
+     * Instanciando serviço
+     * @param buscaCEP serviço instanciado na aplicação
+     */
+    public Recursos(BuscaCEP buscaCEP) {
+        this.buscaCEP = buscaCEP;
+    }
+
+    /**
+     * Faz validacao e busca do CEP
+     * @param numero numero correspondente ao CEP
+     * @return endpoint da aplicação
+     */
+    @GET
+    @Path("/{numero}")
+    @Timed
+    public Response getCEPValido(@Length(min = 8, max = 8, message = "CEP inválido")
+                                    @Digits(integer = 8, fraction = 0, message = "CEP inválido")
+                                    @PathParam("numero") String numero) {
+        Endereco endereco = buscaCEP.getCEPValido(numero);
+        if (endereco == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(endereco).build();
+    }
+}
